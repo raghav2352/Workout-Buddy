98c46389e0eb89926a3175eab737119f35bee7c2
Raghav <raghavpurohit19@gmail.com>
2024-06-05 11:56:30 +0530

    routes creation for CRUD operations

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+require('dotenv').config()
+const express = require('express');
+
+//express app
+const app = express()
+const workoutRoutes = require('./routes/workouts')
+
+//middleware
+app.use(express.json())
+app.use((req,res,next) => {
+    console.log(req.path,req.method)
+    next()
+})
+
+//routes
+app.use('/api/workouts',workoutRoutes)
+
+//listen for requests
+
+app.listen(process.env.PORT, () =>{
+    console.log('listening on port 4000!!!')
+})
