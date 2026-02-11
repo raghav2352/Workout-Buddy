e05aa87b628b6ed58c09bc21b35436687cfb1fd0
raghav <raghavpurohit19@gmail.com>
2026-02-11 10:10:39 +0530

    Created frontend

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ mongoose.connect(process.env.MONGO_URI)
     .then(() => {
         //listen for requests
         app.listen(process.env.PORT, () => {
-            console.log('connected to db & listening on port 4000 ' )
+            console.log('connected to db & listening on port 4001' )
         })
     })
     .catch((error) => {
